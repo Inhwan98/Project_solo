ebdae57909be143aa1e0ec893daec093a51836de
Inhwan98 <asd9613@gmail.com>
2023-06-21 11:21:07 +0900

    Inan new

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float zOffset;
+    [SerializeField] private float xOffset;
+    [SerializeField] private float height;
+    [SerializeField] private float damping = 0.2f;
+
+    Vector3 velocity;
+    PlayerController playerCtr;
+    Transform playerTr;
+    void Start()
+    {  
+       playerCtr = PlayerController.instance; 
+       playerTr = playerCtr.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = playerTr.position +
+                      (-Vector3.forward * zOffset) +
+                      (Vector3.right * xOffset) +
+                       (Vector3.up * height);
+
+        transform.position = Vector3.SmoothDamp(transform.position,
+                                                pos,
+                                                ref velocity,
+                                                damping);
+    }
+}
